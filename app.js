5f6f77baf33c109ddf8b3558d4c3e8100b1cecc8
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-05-08 13:34:53 -0500

    map and users coordinites

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
+var map = L.map('map').setView([42.7550044, -98.809875], 13);
 // Create map:                                                       
-const myMap = {
+// const myMap = {
+//     coordinates: [],
+//     businesses: [],
+//     map: {},
+//     markers: {},
 
-}
-   
-});
+// //build map
+//     buildMap() {
+//         this.map = Lmap('map', {
+//             center: this.coordinates,
+//             zoom: 11,
+//         }),
+//     // Add OpenStreetMap tiles:
 
-// Add OpenStreetMap tiles:
-L.tileLayer('https://.tile.openstreetmap.org/{z}/{x}/{Y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    minZoom: '15',
-}).addTo(myMap)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-// Create and add a geolocation marker:
-const marker = L.marker([])
-marker.addTo(myMap).bindPopup('<p1><b>You are Here</b></p1>').openPopup()
+    //     // Create and add a geolocation marker:
+    L.marker([42.7550044, -98.809875]).addTo(map)
+        .bindPopup('You are here.')
+        .openPopup();
+
+//     },
+// }
+//find user's location
+
+      var div  = document.getElementById("location");
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+          div.innerHTML = "The Browser Does not Support Geolocation";
+        }
+      }
+
+      function showPosition(position) {
+        div.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+      }
+
+      function showError(error) {
+        if(error.PERMISSION_DENIED){
+            div.innerHTML = "The User have denied the request for Geolocation.";
+        }
+      }
+      getLocation();
+    
